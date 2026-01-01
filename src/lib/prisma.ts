bcb141539a07baa3c6364e48b63cbdbe9c6af8ef
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2026-01-01 15:23:12 +0200

    feat: refactor Prisma client initialization to use a factory function

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,12 +1,18 @@
 import { PrismaClient } from "@/generated/prisma/client";
 import { withAccelerate } from "@prisma/extension-accelerate";
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: InstanceType<typeof PrismaClient> | undefined;
+const createPrismaClient = () => {
+  return new PrismaClient({
+    accelerateUrl: process.env.PRISMA_ACCELERATE_URL || ""
+  }).$extends(withAccelerate());
 };
 
-export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient({
-    accelerateUrl: process.env.PRISMA_ACCELERATE_URL || "",
-  }).$extends(withAccelerate());
+type PrismaClientType = ReturnType<typeof createPrismaClient>;
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClientType | undefined;
+};
+
+export const prisma = globalForPrisma.prisma ?? createPrismaClient();
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
